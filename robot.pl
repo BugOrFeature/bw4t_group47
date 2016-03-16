3b73335be1aa67512d26cebc47d6d84ef2a605aa
Tim Wissel <twtimwissel5@gmail.com>
2016-03-16 16:47:19 +0100

    added visited() and block()

--- robot.pl
+++ robot.pl
@@ -7,7 +7,9 @@
 	holding/1,
 	place/1,
 	sequence/1,
-	sequenceIndex/1.
+	sequenceIndex/1,
+	visited/1,
+	block/2.
 
 % A room is a place with exactly one neighbour, i.e., there is only one way to get to and from that place.
 room(PlaceID) :- zone(_,PlaceID,_,_,Neighbours), length(Neighbours,1).
