031923088a106020cbc40761b76b032af6b87c70
BugOrFeature <marceyv@gmail.com>
2016-03-20 21:59:48 +0100

    Fixed everything besides - last room error - not optimized yet

--- robot.pl
+++ robot.pl
@@ -13,6 +13,8 @@
 	
 % A room is a place with exactly one neighbour, i.e., there is only one way to get to and from that place.
 room(PlaceID) :- zone(_,PlaceID,_,_,Neighbours), length(Neighbours,1).
+
+
 nextColorInSeq(Color) :- sequenceIndex(X), sequence(Y), nth0(X, Y, Color).
 
 % Exercise 2.2: insert a definition of the predicate "nextColorInSeq(Color)".
